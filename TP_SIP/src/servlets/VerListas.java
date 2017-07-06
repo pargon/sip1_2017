9e6d5bd425079517dd258088b36069f1bc269315
gparra <gparra@GPARRA-PC.VALKIMIA.COM>
2017-07-06 13:00:13 -0300

    va

--- TP_SIP/src/servlets/VerListas.java
+++ TP_SIP/src/servlets/VerListas.java
@@ -2,63 +2,44 @@ package servlets;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/VerListas")
+import app.Controlador;
+
+@WebServlet("/ListarIntegrantes")
 public class VerListas extends HttpServlet{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-/*		
+		
 		String valor = req.getParameter("action");
 		String destino = "inicio.jsp";
-		BD bd = new BD();		
-		bd.LookUPServ();
 		
-
+		Controlador ctr = new Controlador();
+		
+		
 		switch(valor ){
-		case "VerCli":{
+		case "VerGrupos":{
 			
-			List<ClienteVO> lc = bd.ListarClientes();
-			req.setAttribute("listcli", lc);
-			destino = "VerClientes.jsp";
+			destino = "VerGrupos.jsp";
 			break;}
 		
-		case "VerVeh":{
-			destino = "VerVehiculos.jsp";
-			break;}
-		
-		case "VerRep":{
-			
-			destino = "VerRepuestos.jsp";
-			break;}
-	
-		case "NvaRepa":{
-			
-			List<ClienteVO> lc = bd.ListarClientes();
-			req.setAttribute("listcli", lc);
-			
-			List<VehiculoVO> lv = bd.ListarVehiculos();
-			req.setAttribute("listveh", lv);
-
-			destino = "NuevaRepa.jsp";
-			break;}
+		case "VerEstruc":{
+			destino = "VerEstructura.jsp";
+			break;}		
 		}
 		
 		RequestDispatcher rd = req.getRequestDispatcher(destino);
-		rd.forward(req, resp);
-*/		
+		rd.forward(req, resp);		
 	}
 
 	@Override
