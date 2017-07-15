8e27a8902a7ea6d3e095059fcf44963d957ce5bf
Facundo Arias <facundoam@gmail.com>
2017-07-15 20:47:41 -0300

    asdasd

--- src/app/TestMain.java
+++ src/app/TestMain.java
@@ -12,7 +12,7 @@ public class TestMain {
 		
 		HibernateDAO.getInstancia().persistir(new Grupo(2,"Pachano"));
 		Grupo grupo = ControladorEntregables.getInstancia().buscarGrupoPorNumero(2);
-		grupo.getIntegrantes().add(new Alumno(1020123,"Facundo","Arias",null,null));
+		grupo.getIntegrantes().add(new Alumno(1020123,"Federico","Arias",null,null));
 		grupo.getIntegrantes().add(new Alumno(1212123,"Facundo","Arias",null,null));
 		grupo.getIntegrantes().add(new Alumno(1231234,"Facundo","Arias",null,null));
 		grupo.getEntregables().add(new Entregable());
